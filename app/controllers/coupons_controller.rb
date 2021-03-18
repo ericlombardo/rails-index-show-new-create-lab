89d444b678aa156fe22ccf7efd9ca6ace6d22da8
Eric <eric.m.lombardo@gmail.com>
2021-03-18 14:46:36 -0400

    Build coupon model with create feature

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,5 +1,19 @@
 class CouponsController < ApplicationController
-  def index
+  def index # shows all coupons
     @coupons = Coupon.all
+  end
+
+  def new # show new form 
+  end
+
+  def create # creates new instance, redirects to show page
+    new_coupon = Coupon.create!(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+
+    redirect_to coupon_path(new_coupon)
+  end
+
+  def show # show individual coupon info
+
+    @coupon = Coupon.find_by_id(params[:id])
   end
 end
